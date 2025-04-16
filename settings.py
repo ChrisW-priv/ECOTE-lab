fefd5fe3495086ea2533909a0f20a2650e0df0e3
ChrisW-priv <chris.watras@gmail.com>
2025-04-16 21:16:14 +0200

    make the compiler a separate installable library

--- settings.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings, CliPositionalArg
-from pydantic import Field
-
-
-class Settings(BaseSettings, cli_parse_args=True):
-    input_file: CliPositionalArg[str] = Field(..., description="Path to input XML file")
-    output_dir: str = Field("generated", description="Directory to output C# code")
-
