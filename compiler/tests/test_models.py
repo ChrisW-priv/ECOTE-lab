8bf2e97aa19eee4b5c974064b7058b60eb98c962
ChrisW-priv <chris.watras@gmail.com>
2025-04-16 22:12:15 +0200

    remove old tests and just create some dummy files for now

---
+++ compiler/tests/test_models.py
@@ -0,0 +1,83 @@
+from abc import ABC
+from dataclasses import dataclass
+
+
+@dataclass
+class ClassAttribute:
+    """
+    Represents an Attribute of a C# Class
+    """
+
+    name: str
+    attribute_type: str
+
+
+@dataclass
+class ElementAttribute:
+    """
+    Represents an Attribute of an XML element
+    """
+
+    name: str
+    value: str
+    ref: str | None = None
+
+
+@dataclass
+class Element:
+    """
+    Represents an element in the Abstract Syntax Tree (AST).
+    """
+
+    element_name: str
+    attributes: list[ElementAttribute] | None = None
+    children: list["Element"] | None = None
+
+
+class Token(ABC):
+    """
+    Represents a lexical token produced by the scanner.
+    """
+
+
+@dataclass(slots=True, frozen=True)
+class Symbol(Token):
+    value: str
+
+
+@dataclass(slots=True, frozen=True)
+class Text(Token):
+    value: str
+
+
+@dataclass(slots=True, frozen=True)
+class String(Token):
+    value: str
+
+
+@dataclass
+class Declaration:
+    """
+    Represents an instance declaration in the final code in the Main section
+    """
+
+    id: str
+    instance_name: str
+    class_name: str
+    attributes: list[ElementAttribute]
+
+
+@dataclass
+class Class:
+    """
+    Represents an instance declaration in the final code in the Main section
+    """
+
+    name: str
+    attributes: list[ClassAttribute]
+
+
+@dataclass
+class TypedTree:
+    types: list[Class]
+    declarations: list[Declaration]
